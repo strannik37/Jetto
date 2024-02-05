fdec7fe568c44a842ca8746de92e5691f29fe30a
strannik37 <58545853+strannik37@users.noreply.github.com>
2024-02-05 20:48:22 +0300

    Update start_multi_8.sh

--- start_multi_8.sh
+++ start_multi_8.sh
@@ -3,6 +3,6 @@ npm install
 
 
 while true; do
-  node send_multigpu.js --api tonapi --bin ./pow-miner-cuda --givers 1000 --gpu-count 8 --timeout 4
+  node send_multigpu.js --api tonapi --bin ./pow-miner-cuda --givers 1000 --gpu-count 8 --timeout 15
   sleep 1;
 done;
